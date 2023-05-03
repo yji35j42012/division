850a5b5170b8272f11dbf610943227eb57d6d01c
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2023-05-04 07:50:35 +0800

    檔案同步

--- js/technology.js
+++ js/technology.js
@@ -2,7 +2,7 @@ var technology_detail = document.querySelectorAll("[name=technology_detail]");
 
 for (let i = 0; i < technology_detail.length; i++) {
 	const element = technology_detail[i];
-	element.onclick = function() {
+	element.onclick = function () {
 		location.href = "./technology_detail.html";
 	};
 }
@@ -18,6 +18,15 @@ function isTextOverflowed(element) {
 function checkOverTxtHadnler() {
 	for (let i = 0; i < checkOverTxt.length; i++) {
 		const element = checkOverTxt[i];
+
+		if (isTextOverflowed(element)) {
+			console.log("Text is overflowed");
+			element.classList.add('showMore');
+		} else {
+			return
+		}
+
+
 		console.log("element", element.getAttribute("data-maxline"));
 		var lineNum = element.getAttribute("data-maxline");
 		var lastTxt = element.getAttribute("data-last");
@@ -44,14 +53,145 @@ function checkOverTxtHadnler() {
 
 		//
 		// console.log("element", element);
-		// if (isTextOverflowed(element)) {
-		// 	console.log("Text is overflowed");
-		// } else {
-		// 	console.log("Text is not overflowed");
-		// }
+		
 	}
 }
 
 if (checkOverTxt) {
 	checkOverTxtHadnler();
+}
+
+
+
+var filter_select = document.querySelector("#filter_select");
+var filter_select_txt = document.querySelector("#filter_select_txt");
+var filter_select_item = document.querySelectorAll("#filter_select_ul > li");
+function selectHandler() {
+	filter_select.classList.contains("on") ? filter_select.classList.remove("on") : filter_select.classList.add("on");
+}
+if (filter_select) {
+	filter_select.addEventListener('click', selectHandler);
+}
+
+if (filter_select_item) {
+	for (let i = 0; i < filter_select_item.length; i++) {
+		const element = filter_select_item[i];
+		element.onclick = function () {
+			filter_select_txt.innerHTML = element.innerHTML
+		}
+	}
+}
+
+var filter_search = document.querySelector("#filter_search");
+var nodata = document.querySelector("#nodata");
+var data = document.querySelector("#data");
+
+if (filter_search) {
+	filter_search.onclick = function (params) {
+		if (nodata.style.display == 'none') {
+			nodata.style.display = ''
+			data.style.display = 'none'
+		} else {
+			nodata.style.display = 'none'
+			data.style.display = ''
+		}
+	}
+}
+
+
+var addFilter = document.querySelector("#addFilter");
+var filter_inpGroup = document.querySelector("#filter_inpGroup");
+var filter_inpGroup_items = document.querySelectorAll("#filter_inpGroup > .filter_inpBox");
+var icon_minus = null;
+console.log('filter_inpGroup_items', filter_inpGroup_items);
+if (addFilter) {
+	addFilter.onclick = function () {
+		console.log('add');
+		let idIndex = "filter_inpBox" + filter_inpGroup_items.length
+		const div = document.createElement("div");
+		div.setAttribute("class", "filter_inpBox");
+		div.setAttribute("id", idIndex);
+
+		const divChild1 = document.createElement("ul");
+		divChild1.setAttribute("class", "tab");
+		const divChild1_1 = document.createElement("li");
+		divChild1_1.setAttribute("class", "tab_item on");
+		divChild1_1.innerHTML = 'and'
+		const divChild1_2 = document.createElement("li");
+		divChild1_2.setAttribute("class", "tab_item");
+		divChild1_2.innerHTML = 'or'
+		divChild1.append(divChild1_1);
+		divChild1.append(divChild1_2);
+
+		const divChild2 = document.createElement("label");
+		divChild2.setAttribute("class", "filter_input");
+		const divChild2_1 = document.createElement("i");
+		divChild2_1.setAttribute("class", "icon _search");
+		const divChild2_1_img = document.createElement("img");
+		divChild2_1_img.setAttribute("src", "./images/icon_search_normal.svg");
+		divChild2_1.append(divChild2_1_img)
+		const divChild2_1_inp = document.createElement("input");
+		divChild2_1_inp.setAttribute("type", 'text')
+		divChild2_1_inp.setAttribute("placeholder", '請輸入姓名、現職單位、職級、合作專長、IPC(如：G06K)、技術名')
+		divChild2.append(divChild2_1)
+		divChild2.append(divChild2_1_inp)
+
+		const divChild3 = document.createElement("i");
+		divChild3.setAttribute("class", "icon _minus");
+		divChild3.setAttribute("data-num", filter_inpGroup_items.length);
+		divChild3.setAttribute("name", 'icon_minus');
+		const divChild3_1 = document.createElement("img");
+		divChild3_1.setAttribute("src", "./images/icon_minus_normal.svg");
+		divChild3.append(divChild3_1)
+
+
+		const divChild4 = document.createElement("label");
+		divChild4.setAttribute("id", "filter_select");
+		divChild4.setAttribute("class", "filter_select");
+		const divChild4_1 = document.createElement("span");
+		divChild4_1.setAttribute("id", "filter_select_txt" + filter_inpGroup_items.length);
+		divChild4_1.setAttribute("class", "filter_select_txt");
+		divChild4_1.innerHTML = '標題';
+		const divChild4_2 = document.createElement("ul");
+		divChild4_2.setAttribute("id", "filter_select_ul" + filter_inpGroup_items.length);
+		divChild4_2.setAttribute("class", "filter_select_ul");
+
+		divChild4_2.innerHTML = `<li class="filter_select_li">標題</li>
+								<li class="filter_select_li">摘要及關鍵字</li>
+								<li class="filter_select_li">IPC</li>
+								<li class="filter_select_li">姓名</li>
+								<li class="filter_select_li">現職單位</li>
+								<li class="filter_select_li">職級</li>
+								<li class="filter_select_li">合作專長</li>
+								<li class="filter_select_li">BaseSEQ</li>`;
+
+
+		divChild4.append(divChild4_1)
+		divChild4.append(divChild4_2)
+
+
+		div.append(divChild1)
+		div.append(divChild2)
+		div.append(divChild4)
+		div.append(divChild3)
+		filter_inpGroup.append(div);
+
+		filter_inpGroup_items = document.querySelectorAll("#filter_inpGroup > .filter_inpBox")
+		icon_minus = document.querySelectorAll("[name=icon_minus]")
+		icon_minusHandler()
+	}
+}
+
+
+function icon_minusHandler() {
+	for (let i = 0; i < icon_minus.length; i++) {
+		const element = icon_minus[i];
+		element.onclick = function () {
+			let removeDiv = document.querySelector("#filter_inpBox" + element.getAttribute("data-num"))
+			// console.log(element.getAttribute("data-num"));
+			console.log('removeDiv', removeDiv);
+
+			filter_inpGroup.removeChild(removeDiv)
+		}
+	}
 }
